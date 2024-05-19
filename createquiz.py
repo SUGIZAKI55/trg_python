c99bab6e54f2b0ff9e1d1b654def89bec658fb9c
杉崎聡史 <sugizaki@MacBook-Air-SUGI.local>
2024-05-19 19:44:08 +0900

    関数、変数、引数の確認

--- createquiz.py
+++ createquiz.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 # 問題をファイルから読み込む関数
 def load_questions():
     with open('quiz_questions.txt', 'r', encoding='utf-8') as file:
-        lines = file.read().split('\n\n')
-    questions = []
-    for line in lines:
+        lines = file.read().split('\n\n') #ファイルを読んで、2改行があったら分ける　#配列として分割する関数split \nは改行コード
+    print(f"@=9====={lines=}") #=はlinesという変数名も表示する
+    questions = [] #四角カッコは配列
+    for line in lines: #linesから1こ取り出しlineに入る　複数形を個別する時に使う表記
+        print(f"@=11====={line=}")
         parts = line.split('\n')
         if len(parts) == 4:
             questions.append({'title': parts[0], 'choices': parts[1], 'answer': parts[2], 'explanation': parts[3]})
@@ -38,7 +40,7 @@ def edit_question(question_id):
     return render_template('edit.html', question=questions[question_id], question_id=question_id)
 
 @app.route('/createQuiz')
-def index():
+def index2():
     questions = load_questions()
     return render_template('q_index.html', questions=questions)
 
