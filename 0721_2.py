fb615f74545394a289bcb6f2cfa70565fe94eb62
杉崎聡史 <sugizaki@MacBook-Air-SUGI.local>
2024-07-26 20:30:15 +0900

    過去のファイル整理

--- 0721_2.py
+++
@@ -1,8 +0,0 @@
-fish=["マグロ","イカ","タコ","ウナギ"]
-seikai=["タコ","ウナギ"]
-for sentaku in fish:
-    # print(sentaku)
-    if sentaku in seikai: 
-        print(sentaku,"○")
-    else:
-        print(sentaku,"×")
